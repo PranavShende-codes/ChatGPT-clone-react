d7bec80b3f258defaf523d932927dbee0ff37371
PranavShende-codes <pranavshede40@gmail.com>
2025-02-13 18:26:23 +0530

    Added Chatwindow component

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -1,7 +1,15 @@
 export default function ChatWindow() {
  return (
-  <div>
-   <h1>ChatWindow</h1>
-  </div>
- )
+   <div className="flex flex-col flex-1 p-5">
+     <div className="flex-1 overflowy-y-auto space-y-2">
+       <p className="text-gray-800">User:</p>
+       <p className="text-gray-800">AI</p>
+     </div>
+
+     <div className="flex gap-2 mt-4">
+       <input type="text" placeholder="Message ChatGPT..." className="flex-1 p-2 border-2 rounded" />
+       <button className="bg-black text-white px-4 py-2 rounded-3xl">Send</button>
+     </div>
+   </div>
+ );
 }
